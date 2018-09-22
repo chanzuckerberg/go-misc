6c00ec8f83e0f5ca3237ccfe9ba488ae501c63e9
Ryan King <ryan@theryanking.com>
2018-09-21 20:47:52 -0700

    move slack code to go-misc (#6)
    move slack code to go-misc

---
+++ slack/client.go
@@ -0,0 +1,65 @@
+package slack
+
+import (
+	"fmt"
+
+	slackClient "github.com/nlopes/slack"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+// Client is a slack client
+type Client struct {
+	Slack  *slackClient.Client
+	logger *logrus.Logger
+}
+
+// New returns a webhook client
+func New(token string, logger *logrus.Logger) *Client {
+	client := slackClient.New(token)
+	return &Client{
+		Slack:  client,
+		logger: logger,
+	}
+}
+
+//GetSlackChannelID returns the chanel id from an email
+func (c *Client) GetSlackChannelID(email string) (string, error) {
+	user, err := c.Slack.GetUserByEmail(email)
+	if err != nil {
+		return "", errors.Wrap(err, "could not find slack user for email")
+	}
+	if user == nil {
+		return "", errors.New("email not found")
+	}
+	c.logger.Info(fmt.Sprintf("userID: %s", user.ID))
+	_, _, channelID, err := c.Slack.OpenIMChannel(user.ID)
+	return channelID, errors.Wrap(err, "could not open dm channel with user")
+}
+
+// PostMessage posts a message
+func (c *Client) PostMessage(message Message) error {
+	channelID, err := c.GetSlackChannelID(message.Email)
+	if err != nil {
+		return err
+	}
+	params := slackClient.PostMessageParameters{
+		UnfurlLinks: true,
+		Attachments: message.Attachments,
+	}
+	_, _, err = c.Slack.PostMessage(channelID, message.Text, params)
+	return errors.Wrap(err, "could not post message")
+}
+
+// SendMessageToUser will send the given text to the specified userID.
+func (c *Client) SendMessageToUser(userID, message string) error {
+	_, _, channelID, err := c.Slack.OpenIMChannel(userID)
+	if err != nil {
+		return err
+	}
+	params := slackClient.PostMessageParameters{
+		UnfurlLinks: true,
+	}
+	_, _, err = c.Slack.PostMessage(channelID, message, params)
+	return err
+}
