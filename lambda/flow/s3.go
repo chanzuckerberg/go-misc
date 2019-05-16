1f90e423d874224cb91df5c91f74e9fa06fde261
edulop <elopez@chanzuckerberg.com>
2019-05-16 14:34:44 -0700

    Add a firehose lambda to parse vpc flow logs.

---
+++ lambda/flow/s3.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	dataMessage = "DATA_MESSAGE"
+)
+
+type logStatus string
+
+const (
+	logStatusOK       logStatus = "OK"
+	logStatusNoData   logStatus = "NODATA"
+	logStatusSkipData logStatus = "SKIPDATA"
+)
+
+type augmentedLogEvent struct {
+	Owner     string `json:"owner,omitempty"`
+	LogGroup  string `json:"logGroup,omitempty"`
+	LogStream string `json:"logStream,omitempty"`
+
+	// From cloudwatch
+	ID        string `json:"id,omitempty"`
+	Timestamp int    `json:"timestamp,omitempty"`
+
+	// Flow logs https://docs.aws.amazon.com/vpc/latest/userguide/flow-logs.html
+	Version     int    `json:"version,omitempty"`
+	AccountID   string `json:"account_id,omitempty"`
+	InterfaceID string `json:"interface_id,omitempty"`
+
+	StartTime string `json:"start_time,omitempty"`
+	EndTime   string `json:"end_time,omitempty"`
+
+	LogStatus logStatus `json:"log_status,omitempty"`
+
+	// optional
+	SourceAddress      *string `json:"source_address,omitempty"`
+	DestinationAddress *string `json:"destination_address,omitempty"`
+	SourcePort         *int    `json:"source_port,omitempty"`
+	DestinationPort    *int    `json:"destination_port,omitempty"`
+	Protocol           *int    `json:"protocol,omitempty"`
+	Packets            *int    `json:"packets,omitempty"`
+	Bytes              *int    `json:"bytes,omitempty"`
+	Action             *string `json:"action,omitempty"`
+}
+
+func (al *augmentedLogEvent) clearOptional() {
+	al.SourceAddress = nil
+	al.DestinationAddress = nil
+	al.SourcePort = nil
+	al.DestinationPort = nil
+	al.Protocol = nil
+	al.Packets = nil
+	al.Bytes = nil
+	al.Action = nil
+}
+
+func (al *augmentedLogEvent) populate(message string) (err error) {
+	split := strings.Split(message, " ")
+
+	if len(split) != 14 {
+		return errors.New("Malformed message")
+	}
+
+	al.Version, err = parseAsInt(split[0])
+	if err != nil {
+		return errors.Wrap(err, "Could not parse version")
+	}
+	al.AccountID = split[1]
+	al.InterfaceID = split[2]
+
+	startTime, err := parseAsInt(split[10])
+	if err != nil {
+		return errors.Wrap(err, "Could not parse start time")
+	}
+	al.StartTime = parseTime(startTime)
+
+	endTime, err := parseAsInt(split[11])
+	if err != nil {
+		return errors.Wrap(err, "Could not parse end time")
+	}
+	al.EndTime = parseTime(endTime)
+
+	al.LogStatus = logStatus(split[13])
+	// End here if we don't have more data
+	if al.LogStatus != logStatusOK {
+		al.clearOptional() // Make sure we are not sending stale values
+		return nil
+	}
+
+	al.SourceAddress = optionalString(split[3])
+	al.DestinationAddress = optionalString(split[4])
+
+	al.SourcePort = parseAsOptionalInt(split[5])
+	al.DestinationPort = parseAsOptionalInt(split[6])
+
+	al.Protocol = parseAsOptionalInt(split[7])
+	al.Packets = parseAsOptionalInt(split[8])
+	al.Bytes = parseAsOptionalInt(split[9])
+	al.Action = optionalString(split[12])
+	return nil
+}
+
+func parseTime(t int) string {
+	return time.Unix(int64(t), 0).Format(time.RFC3339)
+}
+
+func parseAsInt(val string) (int, error) {
+	i, err := strconv.Atoi(val)
+	return i, errors.Wrapf(err, "Could not parse %s as int", val)
+}
+
+func parseAsOptionalInt(val string) *int {
+	i, err := parseAsInt(val)
+	if err != nil {
+		logrus.Error()
+		return nil
+	}
+	return optionalInt(i)
+}
+
+func optionalString(s string) *string {
+	return &s
+}
+
+func optionalInt(i int) *int {
+	return &i
+}
+
+func processRecord(record events.KinesisFirehoseEventRecord) (response events.KinesisFirehoseResponseRecord, err error) {
+	response.Data = record.Data
+	response.RecordID = record.RecordID
+	logrus.Infof("Processing record %s", record.RecordID)
+
+	gr, err := gzip.NewReader(bytes.NewBuffer(record.Data))
+	if err != nil {
+		response.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+		return
+	}
+	defer gr.Close()
+
+	parsed := &events.CloudwatchLogsData{}
+	data, err := ioutil.ReadAll(gr)
+	if err != nil {
+		response.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+		return
+	}
+	err = json.Unmarshal(data, parsed)
+	if err != nil {
+		response.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+		return
+	}
+	if parsed.MessageType != dataMessage {
+		response.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+		return
+	}
+	messages := bytes.NewBuffer(nil)
+	b := []byte{}
+
+	// Set some common stuff
+	augmented := augmentedLogEvent{}
+	augmented.Owner = parsed.Owner
+	augmented.LogGroup = parsed.LogGroup
+	augmented.LogStream = parsed.LogStream
+
+	for _, logEvent := range parsed.LogEvents {
+		err = augmented.populate(logEvent.Message)
+		if err != nil {
+			response.Data = messages.Bytes()
+			response.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+			return
+		}
+		b, err = json.Marshal(augmented)
+		if err != nil {
+			response.Data = messages.Bytes()
+			response.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+			return
+		}
+		_, err = messages.Write(b)
+		if err != nil {
+			response.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+			return
+		}
+		_, err = messages.WriteRune('\n')
+		if err != nil {
+			response.Result = events.KinesisFirehoseTransformedStateProcessingFailed
+			return
+		}
+	}
+	response.Data = messages.Bytes()
+	response.Result = events.KinesisFirehoseTransformedStateOk
+	logrus.Infof("Successfully parsed %d messages in recordID %s", len(parsed.LogEvents), record.RecordID)
+	return
+}
+
+// HandleRequest handles a kinesis event request
+func HandleRequest(ctx context.Context, kinesisEvent events.KinesisFirehoseEvent) (events.KinesisFirehoseResponse, error) {
+	response := events.KinesisFirehoseResponse{
+		Records: []events.KinesisFirehoseResponseRecord{},
+	}
+	logrus.Infof("Received %d kinesis records", len(kinesisEvent.Records))
+	for _, record := range kinesisEvent.Records {
+		record, err := processRecord(record)
+		if err != nil {
+			logrus.WithError(err).Error("Error processing records")
+			return response, err
+		}
+		response.Records = append(response.Records, record)
+	}
+	logrus.Info("Success")
+	return response, nil
+}
+
+func main() {
+	logrus.Info("Processing started")
+	lambda.Start(HandleRequest)
+}
---
+++ lambda/flow/s3_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func TestS3ProcessRecord(t *testing.T) {
+	a := assert.New(t)
+
+	in := events.KinesisFirehoseEventRecord{
+		RecordID: "AAA",
+		Data:     []byte{},
+	}
+
+	data := events.CloudwatchLogsData{
+		Owner:       "an owner",
+		LogGroup:    "a log group",
+		LogStream:   "a log stream",
+		MessageType: dataMessage,
+		LogEvents: []events.CloudwatchLogsLogEvent{
+			{
+				ID:        "asfd",
+				Message:   "2 123456789010 eni-abc123de 172.31.16.139 172.31.16.21 20641 22 6 20 4249 1418530010 1418530070 ACCEPT OK",
+				Timestamp: 123123,
+			},
+		},
+	}
+
+	b, err := json.Marshal(data)
+	a.Nil(err)
+
+	compressed := bytes.NewBuffer(nil)
+	g := gzip.NewWriter(compressed)
+	_, err = g.Write(b)
+	a.Nil(err)
+	err = g.Close()
+	a.Nil(err)
+	in.Data = compressed.Bytes()
+
+	out, err := processRecord(in)
+	a.Nil(err)
+
+	a.Equal(out.RecordID, in.RecordID)
+	a.Equal(out.Result, events.KinesisFirehoseTransformedStateOk)
+
+	r := bufio.NewReader(bytes.NewBuffer(out.Data))
+
+	l, _, err := r.ReadLine()
+	a.Nil(err)
+
+	log := &augmentedLogEvent{}
+	err = json.Unmarshal(l, log)
+	a.Nil(err)
+
+	a.Equal(log.LogGroup, data.LogGroup)
+	a.Equal(log.LogStream, data.LogStream)
+	a.Equal(log.Owner, data.Owner)
+
+	// TODO(el): Add all the fields here
+	a.Equal(log.Version, 2)
+	a.Equal(log.AccountID, "123456789010")
+}
