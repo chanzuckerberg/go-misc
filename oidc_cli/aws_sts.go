0319f3a5ccb1d9e83b6b2cfef8d4ec94f312b239
edulop <elopez@chanzuckerberg.com>
2020-05-06 09:04:15 -0400

    [feature] AWS OIDC token fetcher also allows us to fetch the OIDC token

--- oidc_cli/aws_sts.go
+++ oidc_cli/aws_sts.go
@@ -1,6 +1,9 @@
 package oidc
 
 import (
+	"context"
+	"sync"
+
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/service/sts/stsiface"
@@ -17,30 +20,52 @@ type AwsOIDCCredsProviderConfig struct {
 	}
 }
 
+// AWSOIDCCredsProvider providers OIDC tokens and aws:STS credentials
+type AWSOIDCCredsProvider struct {
+	credentials.ProviderWithContext
+
+	fetcher *tokenFetcher
+}
+
+// FetchOIDCToken will fetch an oidc token
+func (a *AWSOIDCCredsProvider) FetchOIDCToken(ctx context.Context) ([]byte, error) {
+	return a.fetcher.FetchToken(ctx)
+}
+
 // NewAWSOIDCCredsProvider returns an AWS credential provider
 // using OIDC.
 func NewAwsOIDCCredsProvider(
 	svc stsiface.STSAPI,
 	conf *AwsOIDCCredsProviderConfig,
-) credentials.Provider {
+) *AWSOIDCCredsProvider {
 
 	tokenFetcher := &tokenFetcher{
 		conf: conf,
 	}
 
-	return stscreds.NewWebIdentityRoleProviderWithToken(
+	provider := stscreds.NewWebIdentityRoleProviderWithToken(
 		svc,
 		conf.AWS.roleARN,
 		conf.AWS.sessionName,
 		tokenFetcher,
 	)
+
+	return &AWSOIDCCredsProvider{
+		ProviderWithContext: provider,
+		fetcher:             tokenFetcher,
+	}
 }
 
 type tokenFetcher struct {
 	conf *AwsOIDCCredsProviderConfig
+	mu   sync.Mutex
 }
 
+// safe for concurrent use
 func (tf *tokenFetcher) FetchToken(ctx credentials.Context) ([]byte, error) {
+	tf.mu.Lock()
+	defer tf.mu.Unlock()
+
 	token, err := GetToken(ctx, tf.conf.OIDC.clientID, tf.conf.OIDC.issuerURL)
 	if err != nil {
 		return nil, err
