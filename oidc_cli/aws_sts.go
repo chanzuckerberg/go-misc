d8a8c40aa2ad6475166a36fecf00547fc4763931
Eduardo Lopez <elopez@chanzuckerberg.com>
2020-05-06 13:04:13 -0400

    [feature] AWS OIDC token fetcher also allows us to fetch the OIDC token (#52)

--- oidc_cli/aws_sts.go
+++ oidc_cli/aws_sts.go
@@ -1,9 +1,13 @@
 package oidc
 
 import (
+	"context"
+	"sync"
+
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/service/sts/stsiface"
+	"github.com/chanzuckerberg/go-misc/oidc_cli/client"
 )
 
 type AwsOIDCCredsProviderConfig struct {
@@ -17,33 +21,61 @@ type AwsOIDCCredsProviderConfig struct {
 	}
 }
 
+// AWSOIDCCredsProvider providers OIDC tokens and aws:STS credentials
+type AWSOIDCCredsProvider struct {
+	*credentials.Credentials
+
+	fetcher *tokenFetcher
+}
+
+// FetchOIDCToken will fetch an oidc token
+func (a *AWSOIDCCredsProvider) FetchOIDCToken(ctx context.Context) (*client.Token, error) {
+	return a.fetcher.fetchFullToken(ctx)
+}
+
 // NewAWSOIDCCredsProvider returns an AWS credential provider
 // using OIDC.
 func NewAwsOIDCCredsProvider(
 	svc stsiface.STSAPI,
 	conf *AwsOIDCCredsProviderConfig,
-) credentials.Provider {
+) *AWSOIDCCredsProvider {
 
 	tokenFetcher := &tokenFetcher{
 		conf: conf,
 	}
 
-	return stscreds.NewWebIdentityRoleProviderWithToken(
+	provider := stscreds.NewWebIdentityRoleProviderWithToken(
 		svc,
 		conf.AWS.roleARN,
 		conf.AWS.sessionName,
 		tokenFetcher,
 	)
+
+	return &AWSOIDCCredsProvider{
+		Credentials: credentials.NewCredentials(provider),
+		fetcher:     tokenFetcher,
+	}
 }
 
 type tokenFetcher struct {
 	conf *AwsOIDCCredsProviderConfig
+	mu   sync.Mutex
 }
 
-func (tf *tokenFetcher) FetchToken(ctx credentials.Context) ([]byte, error) {
-	token, err := GetToken(ctx, tf.conf.OIDC.clientID, tf.conf.OIDC.issuerURL)
+// safe for concurrent use
+// fetches a full token
+func (tf *tokenFetcher) fetchFullToken(ctx context.Context) (*client.Token, error) {
+	tf.mu.Lock()
+	defer tf.mu.Unlock()
+
+	return GetToken(ctx, tf.conf.OIDC.clientID, tf.conf.OIDC.issuerURL)
+}
+
+func (tf *tokenFetcher) FetchToken(ctx context.Context) ([]byte, error) {
+	token, err := tf.fetchFullToken(ctx)
 	if err != nil {
 		return nil, err
 	}
+
 	return []byte(token.IDToken), nil
 }
