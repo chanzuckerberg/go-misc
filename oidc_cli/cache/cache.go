c995aa16b6e68d56acc2e1fd664a27175914cb5c
Eduardo Lopez <elopez@chanzuckerberg.com>
2020-06-17 15:15:14 -0400

    [feature] Improve oidc pid locking (#63)
    Instead of acquiring an exclusive lock on each invocation of oidc clients, we only do it when we need to refresh tokens. This significantly speeds up code that doesn't efficiently re-use session/credentials (such as terraform).

--- oidc_cli/cache/cache.go
+++ oidc_cli/cache/cache.go
@@ -5,6 +5,7 @@ import (
 
 	client "github.com/chanzuckerberg/go-misc/oidc_cli/client"
 	"github.com/chanzuckerberg/go-misc/oidc_cli/storage"
+	"github.com/chanzuckerberg/go-misc/pidlock"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -12,6 +13,7 @@ import (
 // Cache to cache credentials
 type Cache struct {
 	storage storage.Storage
+	lock    *pidlock.Lock
 
 	refreshToken func(context.Context, *client.Token) (*client.Token, error)
 }
@@ -20,33 +22,50 @@ type Cache struct {
 func NewCache(
 	storage storage.Storage,
 	refreshToken func(context.Context, *client.Token) (*client.Token, error),
+	lock *pidlock.Lock,
 ) *Cache {
 	return &Cache{
 		storage:      storage,
 		refreshToken: refreshToken,
+		lock:         lock,
 	}
 }
 
 // Read will attempt to read a token from the cache
 //      if not present or expired, will refresh
 func (c *Cache) Read(ctx context.Context) (*client.Token, error) {
-	cached, err := c.storage.Read(ctx)
+	// If we have a fresh token, use it
+	cachedToken, err := c.readFromStorage(ctx)
 	if err != nil {
 		return nil, err
 	}
-	cachedToken, err := client.TokenFromString(cached)
-	if err != nil {
-		logrus.Debugf("error fetching stored token: %s", err)
-		err = c.storage.Delete(ctx) // can't read it, so attempt to purge it
-		if err != nil {
-			logrus.Debugf("error clearing token from storage: %s", err)
-		}
-	}
-
-	if cachedToken.IsFresh() {
+	if cachedToken != nil {
 		return cachedToken, nil
 	}
 
+	// otherwise, try refreshing
+	return c.refresh(ctx, cachedToken)
+}
+
+func (c *Cache) refresh(ctx context.Context, cachedToken *client.Token) (*client.Token, error) {
+	err := c.lock.Lock()
+	if err != nil {
+		return nil, err
+	}
+	defer c.lock.Unlock()
+
+	// acquire lock, try reading from cache again just in case
+	// someone else got here first
+	cachedToken, err = c.readFromStorage(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if cachedToken != nil {
+		return cachedToken, nil
+	}
+
+	// ok, at this point we have the lock and there are no good tokens around
+	// fetch a new one and save it
 	token, err := c.refreshToken(ctx, cachedToken)
 	if err != nil {
 		return nil, err
@@ -67,4 +86,23 @@ func (c *Cache) Read(ctx context.Context) (*client.Token, error) {
 	}
 
 	return token, nil
+}
+
+func (c *Cache) readFromStorage(ctx context.Context) (*client.Token, error) {
+	cached, err := c.storage.Read(ctx)
+	if err != nil {
+		return nil, err
+	}
+	cachedToken, err := client.TokenFromString(cached)
+	if err != nil {
+		logrus.Debugf("error fetching stored token: %s", err)
+		err = c.storage.Delete(ctx) // can't read it, so attempt to purge it
+		if err != nil {
+			logrus.Debugf("error clearing token from storage: %s", err)
+		}
+	}
+	if cachedToken.IsFresh() {
+		return cachedToken, nil
+	}
+	return nil, nil
 }
