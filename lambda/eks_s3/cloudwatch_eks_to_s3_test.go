c0c585374c45667b3465e03801de4254b1b606c3
Sandeep Mandala <smandala@chanzuckerberg.com>
2020-07-30 12:58:59 -0700

    fix CI errors to remove duplicate lines in test code

--- lambda/eks_s3/cloudwatch_eks_to_s3_test.go
+++ lambda/eks_s3/cloudwatch_eks_to_s3_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
@@ -11,71 +10,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestS3ProcessRecordOK(t *testing.T) {
-	a := assert.New(t)
-
-	in := events.KinesisFirehoseEventRecord{
-		RecordID: "AAA",
-		Data:     []byte{},
-	}
-
-	data := events.CloudwatchLogsData{
-		Owner:       "an owner",
-		LogGroup:    "/aws/eks/some-cluster-name/cluster",
-		LogStream:   "a log stream",
-		MessageType: dataMessage,
-		LogEvents: []events.CloudwatchLogsLogEvent{
-			{
-				ID:        "asfd",
-				Message:   "asfd",
-				Timestamp: 123123,
-			},
-		},
-	}
-
-	b, err := json.Marshal(data)
-	a.Nil(err)
-
-	compressed := bytes.NewBuffer(nil)
-	g := gzip.NewWriter(compressed)
-	_, err = g.Write(b)
-	a.Nil(err)
-	err = g.Close()
-	a.Nil(err)
-	in.Data = compressed.Bytes()
-
-	out, err := processRecord(in)
-	a.Nil(err)
-
-	a.Equal(out.RecordID, in.RecordID)
-	a.Equal(out.Result, events.KinesisFirehoseTransformedStateOk)
-
-	r := bufio.NewReader(bytes.NewBuffer(out.Data))
-
-	l, _, err := r.ReadLine()
-	a.Nil(err)
-
-	log := &augmentedLogEvent{}
-	err = json.Unmarshal(l, log)
-	a.Nil(err)
-
-	a.Equal(log.LogGroup, data.LogGroup)
-	a.Equal(log.LogStream, data.LogStream)
-	a.Equal(log.Owner, data.Owner)
-}
-
 func TestS3ProcessRecordDropped(t *testing.T) {
 	a := assert.New(t)
 
 	in := events.KinesisFirehoseEventRecord{
-		RecordID: "AAA",
+		RecordID: "record1",
 		Data:     []byte{},
 	}
 
 	data := events.CloudwatchLogsData{
-		Owner:       "an owner",
-		LogGroup:    "some log group",
-		LogStream:   "a log stream",
+		Owner:       "owner1",
+		LogGroup:    "loggroup",
 		MessageType: dataMessage,
 		LogEvents: []events.CloudwatchLogsLogEvent{
 			{
