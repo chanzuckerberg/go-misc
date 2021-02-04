39655f55a842f3e9990baf80f09835dae0f0cef5
Annie <anne.ku@chanzuckerberg.com>
2021-02-03 19:10:11 -0800

    latest progress and confusion

--- lambda/rotator-snowflake/go.mod
+++ lambda/rotator-snowflake/go.mod
@@ -3,7 +3,9 @@ module github.com/chanzuckerberg/go-misc/lambda/rotator-snowflake
 go 1.15
 
 require (
-	github.com/chanzuckerberg/go-misc v0.0.0-20210122153822-f77c57f96a1c
+	github.com/chanzuckerberg/go-misc v0.0.0-20210202193829-8fe43edc29d6
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.6.1
+	github.com/xinsnake/databricks-sdk-golang v0.1.3
 )
--- lambda/rotator-snowflake/main.go
+++ lambda/rotator-snowflake/main.go
@@ -3,30 +3,43 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
+	"github.com/chanzuckerberg/go-misc/snowflake"
 	"github.com/sirupsen/logrus"
+	databricks "github.com/xinsnake/databricks-sdk-golang"
 )
 
 type sink func() error // TODO: figure out inputs & outputs (pub key, priv key, user & snowflake account)
 
-func source(sourceDetails ...string) (map[string]string, error) {
+func configureSnowflake(sourceDetails ...snowflake.SnowflakeConfig) (map[string]string, error) {
 	// Generate keypair, write to snowflake
 	for _, source := range sourceDetails {
 		fmt.Println(source)
+		// Connect to snowflake
+
+		// Execute snowflake query
+
+		//
 	}
+
 	// Returns the keypair
 	return nil, nil
 } // TODO: figure out inputs & outputs (pub key, priv key, user & snowflake account)
 
 func databricksSink(sinkInputs map[string]string) error {
-	// Same signature as sink type
+	// Connect to Databricks
+	var o databricks.DBClientOption
+	o.Host = os.Getenv("DATABRICKS_HOST")
+	o.Token = os.Getenv("DATABRICKS_TOKEN")
 
-	// Connecting to databricks
 	return nil
 }
 
 func Run(ctx context.Context) error {
-	sourceOutputs, err := source("account1")
+	// read snowflake YAML file? or command line arguments?
+	inputConfiguration := snowflake.SnowflakeConfig{}
+	sourceOutputs, err := configureSnowflake(inputConfiguration)
 	if err != nil {
 		return err
 	}
