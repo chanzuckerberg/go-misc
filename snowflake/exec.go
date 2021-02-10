3512c3caea6d0871e25256704c5804d2f9161e4f
Annie <anne.ku@chanzuckerberg.com>
2021-02-10 12:06:46 -0800

    change function name to include no rows

--- snowflake/exec.go
+++ snowflake/exec.go
@@ -6,7 +6,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func Exec(db *sql.DB, query string) (sql.Result, error) {
+func ExecNoRows(db *sql.DB, query string) (sql.Result, error) {
 	logrus.Debug("[DEBUG] exec stmt ", query)
 
 	return db.Exec(query)
