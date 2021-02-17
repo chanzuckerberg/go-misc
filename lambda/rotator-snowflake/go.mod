dfbab1c762a1efc1483a97470784e3f6b999a025
Annie <anne.ku@chanzuckerberg.com>
2021-02-16 16:13:21 -0800

    add lambda functionality

--- lambda/rotator-snowflake/go.mod
+++ lambda/rotator-snowflake/go.mod
@@ -3,7 +3,8 @@ module github.com/chanzuckerberg/go-misc/lambda/rotator-snowflake
 go 1.15
 
 require (
-	github.com/chanzuckerberg/go-misc v0.0.0-20210212202630-d2f338e0cfd8
+	github.com/aws/aws-lambda-go v1.19.1
+	github.com/chanzuckerberg/go-misc v0.0.0-20210216201956-3b727ddc8f48
 	github.com/davecgh/go-spew v1.1.1
 	github.com/hashicorp/go-multierror v1.1.0
 	github.com/pkg/errors v0.9.1
--- lambda/rotator-snowflake/main.go
+++ lambda/rotator-snowflake/main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"context"
+	"flag"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/chanzuckerberg/go-misc/lambda/rotator-snowflake/rotate"
 	"github.com/sirupsen/logrus"
 )
+
+var localFlag = flag.Bool("local", false, "Whether this lambda should be run locally")
 
 func Run(ctx context.Context) error {
 	return rotate.Rotate(ctx)
 }
 
 func main() {
-	if err := Run(context.Background()); err != nil {
+	logrus.SetLevel(logrus.DebugLevel)
+	flag.Parse()
+	logrus.Debugf("localFlag: %t", *localFlag)
+
+	// local-mode for lambda
+	if *localFlag {
+		err := Run(context.Background())
 		logrus.Fatal(err)
+	} else {
+		lambda.Start(Run)
 	}
 }
