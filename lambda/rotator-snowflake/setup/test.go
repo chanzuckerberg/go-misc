2139ce1980086e29f01d912e075cd5b699bc5152
Annie <anne.ku@chanzuckerberg.com>
2021-03-10 16:25:00 -0800

    save changes for now

--- lambda/rotator-snowflake/setup/test.go
+++ lambda/rotator-snowflake/setup/test.go
@@ -1,4 +1,4 @@
-package snowflake
+package main
 
 import (
 	"database/sql"
@@ -6,8 +6,28 @@ import (
 	"testing"
 
 	"github.com/chanzuckerberg/aws-oidc/pkg/util"
+	"github.com/chanzuckerberg/go-misc/lambda/rotator-snowflake/setup"
 	"github.com/stretchr/testify/require"
+	"github.com/xinsnake/databricks-sdk-golang/aws"
 )
+
+func TestDatabricksSetup(t *testing.T) {
+	r := require.New(t)
+	defer util.ResetEnv(os.Environ())
+	err := os.Setenv("DATABRICKS_HOST", "testhost")
+	r.NoError(err)
+	os.Setenv("DATABRICKS_TOKEN", "testtoken")
+	r.NoError(err)
+	clientPtr, err := Databricks()
+	r.NoError(err)
+	r.NotNil(clientPtr)
+	r.IsType(&aws.DBClient{}, clientPtr)
+
+	os.Unsetenv("DATABRICKS_TOKEN")
+	clientPtr, err = Databricks()
+	r.Error(err)
+	r.Nil(clientPtr)
+}
 
 func TestSnowflakeSetup(t *testing.T) {
 	r := require.New(t)
@@ -22,7 +42,7 @@ func TestSnowflakeSetup(t *testing.T) {
 	err = os.Setenv("SNOWFLAKE_REGION", "testregion")
 	r.NoError(err)
 
-	dbPtr, err := Snowflake("testAccount")
+	dbPtr, err := setup.Snowflake("testAccount")
 	r.NoError(err)
 	r.NotNil(dbPtr)
 	r.IsType(&sql.DB{}, dbPtr)
