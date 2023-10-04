87ccafbcb30377e76d435c23f701c8fe0b5006e3
Annie Ku <anne.ku@chanzuckerberg.com>
2023-10-04 09:57:08 -0700

    wip: attempt at making the oidc_impl package more general

--- oidc_cli/oidc_impl/client/client.go
+++ oidc_cli/oidc_impl/client/client.go
@@ -223,7 +223,7 @@ func (c *Client) Authenticate(ctx context.Context) (*Token, error) {
 	}
 
 	c.server.Start(ctx, c, oauthMaterial)
-	fmt.Fprintf(os.Stderr, "Opening browser in order to authenticate with Okta, hold on a brief second...\n")
+	fmt.Fprintf(os.Stderr, "Opening browser in order to authenticate with your Identity Provider, hold on a brief second...\n")
 	time.Sleep(2 * time.Second)
 
 	// intercept these outputs, send them back on error
