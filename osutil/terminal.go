9607fefc3d7084eeac4b26feba1e6efa3c4177b7
Jake Heath <jheath@chanzuckerberg.com>
2025-12-05 11:17:17 -0800

    fmt

--- osutil/terminal.go
+++ osutil/terminal.go
@@ -6,30 +6,30 @@ import (
 )
 
 func IsDesktopEnvironment() bool {
-    switch runtime.GOOS {
-    case "darwin":
-        // macOS always has a desktop environment if it's running
-        return true
-    
-    case "linux":
-        // Check for GNOME specifically
-        if os.Getenv("XDG_CURRENT_DESKTOP") == "GNOME" {
-            return true
-        }
-        
-        // More permissive: check for any desktop environment
-        if os.Getenv("XDG_CURRENT_DESKTOP") != "" {
-            return true
-        }
-        
-        // Check for X11 or Wayland display servers
-        if os.Getenv("DISPLAY") != "" || os.Getenv("WAYLAND_DISPLAY") != "" {
-            return true
-        }
-        
-        return false
-    
-    default:
-        return false
-    }
+	switch runtime.GOOS {
+	case "darwin":
+		// macOS always has a desktop environment if it's running
+		return true
+
+	case "linux":
+		// Check for GNOME specifically
+		if os.Getenv("XDG_CURRENT_DESKTOP") == "GNOME" {
+			return true
+		}
+
+		// More permissive: check for any desktop environment
+		if os.Getenv("XDG_CURRENT_DESKTOP") != "" {
+			return true
+		}
+
+		// Check for X11 or Wayland display servers
+		if os.Getenv("DISPLAY") != "" || os.Getenv("WAYLAND_DISPLAY") != "" {
+			return true
+		}
+
+		return false
+
+	default:
+		return false
+	}
 }
