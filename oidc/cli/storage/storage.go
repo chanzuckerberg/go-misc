cf42cc56c867f3fbd75bb985395c0e0ae3002259
Jake Heath <jheath@chanzuckerberg.com>
2026-01-30 13:51:23 -0800

    go

--- oidc/cli/storage/storage.go
+++ oidc/cli/storage/storage.go
@@ -41,7 +41,7 @@ func GetOIDC(clientID string, issuerURL string) (Storage, error) {
 		return getFileStorage(clientID, issuerURL)
 	}
 
-	return NewKeyring(clientID, issuerURL), nil
+	return getFileStorage(clientID, issuerURL)
 }
 
 func getFileStorage(clientID string, issuerURL string) (Storage, error) {
